a86e228de47fc3372c7cb65b05dce5fc3690e210
Krrish Dayal <krrish.dayal04@gmail.com>
2025-04-17 13:46:06 +0500

    Add files via upload

---
+++ SetOperations.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+    public static void main(String[] args) {
+        // Create two sets
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        // Add elements to set1
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+
+        // Add elements to set2
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+
+        // Union
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+        System.out.println("Union: " + union);
+
+        // Intersection
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        System.out.println("Intersection: " + intersection);
+
+        // Difference (set1 - set2)
+        Set<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        System.out.println("Difference (set1 - set2): " + difference);
+    }
+}
